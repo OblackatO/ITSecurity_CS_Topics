67e88a15ea1b1fba0bfa6e841719d1ef0b3fd7e6
Pedro Gomes <pegom0896@zoho.eu>
2022-01-26 22:13:54 +0100

    added my version of BFS

--- bfs.py
+++ bfs.py
@@ -9,7 +9,7 @@ BSF overview.
 
 from typing import Dict, List
 
-graph: Dict[str, List[str]] = {
+graph = {
   '5' : ['3','7'],
   '3' : ['2', '4'],
   '7' : ['8'],
@@ -18,16 +18,48 @@ graph: Dict[str, List[str]] = {
   '8' : []
 }
 
-
-def get_queue(graph):
+"""
+My version.
+"""
+def bfs_my_version(graph):
     queue = []
-    for vertice in graph:
-        if vertice not in queue:
-            queue.append(vertice)
-        for child in graph[vertice]:
-            if child not in queue:
-                queue.append(child)
-    return queue
+    visited_nodes = []
+
+    queue.append(list(graph.items())[0][0])
+    visited_nodes.append(list(graph.items())[0][0])
+
+    while queue:
+        q_head = queue.pop(0) # Gets head of queue
+        for child in graph[q_head]:
+            if child not in visited_nodes:
+                queue.append(child) # Child added to the queue if not yet visited, so we can get his children.
+                visited_nodes.append(child)
+    return visited_nodes
+
+
+"""
+https://favtutor.com/blogs/breadth-first-search-python version
+"""
+visited = [] # List for visited nodes.
+queue = []     #Initialize a queue
+def bfs(visited, graph, node): #function for BFS
+  visited.append(node)
+  queue.append(node)
+
+  while queue:          # Creating loop to visit each node
+    m = queue.pop(0)
+    print (m, end = " ")
+
+    for neighbour in graph[m]:
+      if neighbour not in visited:
+        visited.append(neighbour)
+        queue.append(neighbour)
+
+# Driver Code
+#print("Following is the Breadth-First Search")
+#bfs(visited, graph, '5')    # function calling
+
 
 if __name__ == "__main__":
-    print(f"BFS queue {get_queue(graph)}")
+    print(f"BFS queue {bfs_my_version(graph)}")
+    bfs(visited, graph, '5')
