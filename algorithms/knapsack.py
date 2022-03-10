3392f3474e6b28f0636038a67a4b1bd3adbd7e63
Pedro Gomes <pegom0896@zoho.eu>
2022-03-10 13:03:59 +0100

    added functional knapsack algo

--- algorithms/knapsack.py
+++ algorithms/knapsack.py
@@ -34,11 +34,8 @@ def build_weights_table(list_items:List[KnapSackItem]) -> List[int]:
         item_weight = list_items[item_index].weight
         item_value = list_items[item_index].value
         for weight_index in range(0, KnapSackItem.BAG_MAX_WEIGHT+1):
-            if weight_index != 0 and weight_index >= item_weight:
-                optimal_value = max(
-                    optimal_weight[item_index-1][weight_index],
-                    item_value + optimal_weight[item_index-1][weight_index-item_weight]
-                )
+            if weight_index != 0 and weight_index >= item_weight and optimal_weight[item_index-1][weight_index] < item_value + optimal_weight[item_index-1][weight_index-item_weight]:
+                optimal_value = item_value + optimal_weight[item_index-1][weight_index-item_weight]
                 optimal_weight_hits[item_index].append(True)
             else:
                 optimal_value = optimal_weight[item_index-1][weight_index]
@@ -48,10 +45,10 @@ def build_weights_table(list_items:List[KnapSackItem]) -> List[int]:
 
 def print_selected_items(boolean_list:List[boolean], items_list:List[KnapSackItem]):
     max_weight = KnapSackItem.BAG_MAX_WEIGHT
-    for item_index in range(len(boolean_list)-1, 1, -1):
+    for item_index in range(len(boolean_list)-1, 0, -1):
         #print(item_index, max_weight)
         if boolean_list[item_index][max_weight]:
-            print(f"{item_index}", end='+')
+            print(f"Selected item number: {item_index}")
             max_weight -= items_list[item_index].weight
 
 items_list = [
@@ -63,6 +60,8 @@ items_list = [
 items_list.insert(0, KnapSackItem(0,0)) # First element is 0,0 -> Covers the 'no objects' case.
 results = build_weights_table(items_list)
 print(f"Maximized value: {results[0][-1][-1]}")
+print(f"Full table: {results[0]}")
+print(f"Full boolean table: {results[1]}")
 print_selected_items(boolean_list=results[1], items_list=items_list)
 
 
